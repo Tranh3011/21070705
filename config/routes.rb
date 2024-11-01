0576af9b1ad9d443fab5130c7fb356065535ca40
Tranh3011 <nguyentra3011@gmail.com>
2024-11-01 08:24:22 +0700

    Polishing plate boiler content

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "books#index"
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
